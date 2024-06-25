f5398679d02cdf2b8832a8dd9039bc48757f600d
Dimitar Cucanov <dimitar.chuchanov@students.finki.ukim.mk>
2024-06-26 00:50:05 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VP_Ptoject
+{
+    public partial class Form1 : Form
+    {
+        private HangmanWord hangmanWord;
+        private int time = 120;
+        public Form1()
+        {
+            InitializeComponent();
+            hangmanWord = new HangmanWord("Dimitar");
+            hangmanWord.MaxFailedAttempts = 3;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            formatTime();
+            time--;
+        }
+
+        private void formatTime()
+        {
+            int min = time / 60;
+            int sec = time % 60;
+            if (min == 0 && sec <= 20) lbl_Time.ForeColor = Color.Red;
+            lbl_Time.Text = String.Format("{0:00}:{1:00}", min, sec);
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            btn_Start.Visible = false;
+            toolStrip1.Visible = false;
+            timer1.Start();
+        }
+
+        private void btn_Ok_Click(object sender, EventArgs e)
+        {
+            if (tb_Attempt.Text.Length == 1)
+            {
+                hangmanWord.GuessLetter(tb_Attempt.Text.ToCharArray().First());
+                if(hangmanWord.Misses == hangmanWord.MaxFailedAttempts)
+                {
+                    if (MessageBox.Show("Play again?", "Game over", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        //scene = new Scene();
+                        //Invalidate();
+                        time = 120;
+                        formatTime();
+                    }
+                }
+                lbl_Word.Text = hangmanWord.UpdateWordState();
+                lbl_AllLeters.Text = hangmanWord.UpdateAlphabet();
+            }
+            else MessageBox.Show("Put a letter in the text box!");
+        }
+
+        private void easy_Difficulty_Click(object sender, EventArgs e)
+        {
+            time = 120;
+            hangmanWord.MaxFailedAttempts = hangmanWord.MaxNumOfMiss[2];
+        }
+
+        private void normal_Difficulty_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HangmanWord.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VP_Ptoject
+{
+    public class HangmanWord
+    {
+        public string Word { get; set; }
+        public HashSet<char> wordLetters { get; set; } = new HashSet<char>();
+        public HashSet<char> AttemptedLetters { get; set; } = new HashSet<char>();
+        public int Misses { get; set; } = 0;
+        public int[] MaxNumOfMiss { get; set; } = { 3, 5, 7 };
+
+        public int MaxFailedAttempts { get; set; }
+
+        public HangmanWord(string word)
+        {
+            Word = word;
+
+            foreach (char letter in Word.ToCharArray())
+            {
+                wordLetters.Add(Char.ToLower(letter));
+            }
+        }
+
+        public void GuessLetter(Char letter)
+        {
+            letter = Char.ToLower(letter);
+            if (wordLetters.Contains(letter))
+            {
+                wordLetters.Remove(letter);
+                AttemptedLetters.Add(letter);
+            }
+            else {
+                AttemptedLetters.Add(letter);
+                Misses++;
+            }
+           
+
+        }
+
+        public string UpdateAlphabet()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                if (AttemptedLetters.Contains(i)) sb.Append(i);
+                else sb.Append("_");
+
+                sb.Append(" ");
+            }
+
+            return sb.ToString();
+        }
+
+        public string UpdateWordState()
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+            foreach(char letter in Word)
+            {
+                char tmp_letter = Char.ToLower(letter);
+                if (wordLetters.Contains(tmp_letter)) sb.Append(tmp_letter);
+                else sb.Append("_");
+
+                sb.Append(" ");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
