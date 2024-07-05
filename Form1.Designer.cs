02b8adfcb24675c23dcfe28e3cb5feb4cfd72aeb
Dimitar Cucanov <dimitar.chuchanov@students.finki.ukim.mk>
2024-07-05 21:17:02 +0200

    Update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -475,6 +475,7 @@ namespace VP_Ptoject
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(817, 450);
             this.Controls.Add(this.lbl_Attempts);
             this.Controls.Add(this.label1);
